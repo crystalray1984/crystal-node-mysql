72396176cce0cd7edb925706a9b17afe045d13ac
郑磊 <crystalray@qq.com>
2019-04-03 11:15:04 +0800

    update

--- index.d.ts
+++ index.d.ts
@@ -1,81 +1,79 @@
 import Mysql from 'mysql'
 
-export declare interface ResultSetHeader {
-    fieldCount: number
-    affectedRows: number
-    insertId: number
-    info: string
-    serverStatus: number
-    warningStatus: number
+declare namespace NodeMysql {
+    interface ResultSetHeader {
+        fieldCount: number
+        affectedRows: number
+        insertId: number
+        info: string
+        serverStatus: number
+        warningStatus: number
+    }
+
+    type QueryResult = ResultSetHeader | any[]
+
+    interface Streaming {
+        streamQuery(query: string, params?: any): Mysql.Query
+        streamQuery(query: Mysql.QueryOptions): Mysql.Query
+        streamExecute(query: string, params?: any): Mysql.Query
+        streamExecute(query: Mysql.QueryOptions): Mysql.Query
+    }
+
+    interface PromiseStreaming {
+        streamQuery(query: string, params?: any): Promise<Mysql.Query>
+        streamQuery(query: Mysql.QueryOptions): Promise<Mysql.Query>
+        streamExecute(query: string, params?: any): Promise<Mysql.Query>
+        streamExecute(query: Mysql.QueryOptions): Promise<Mysql.Query>
+    }
+
+    interface Closable {
+        end(): Promise<void>
+    }
+
+    interface Queryable extends Mysql.EscapeFunctions {
+        query(query: string, params?: any): Promise<QueryResult>
+        query(query: Mysql.QueryOptions): Promise<QueryResult>
+
+        execute(query: string, params?: any): Promise<QueryResult>
+        execute(query: Mysql.QueryOptions): Promise<QueryResult>
+
+        CONSTRANTS: Constrants
+    }
+
+    function escape(value: any, stringifyObjects?: boolean, timeZone?: string): string
+
+    function escapeId(value: string, forbidQualified?: boolean): string
+
+    function format(sql: string, values: any[], stringifyObjects?: boolean, timeZone?: string): string
+
+    function raw(sql: string): () => string
+
+    interface TransactionQuery extends Queryable, Streaming, Closable {
+        commit(): Promise<void>
+        rollback(): Promise<void>
+    }
+
+    type TransactionScope<T> = (conn: Queryable) => Promise<T>
+
+    interface Pool extends Queryable, Closable, PromiseStreaming {
+        beginTransaction(isoLevel?: string): Promise<TransactionQuery>
+        beginTransaction<T>(scope: TransactionScope<T>): Promise<T>
+        beginTransaction<T>(isoLevel: string, scope: TransactionScope<T>): Promise<T>
+    }
+
+    function createPool(options: Mysql.PoolConfig | string): Pool
+
+    interface RawValue {
+        toSqlString(): string
+    }
+
+    const CONSTRANTS: {
+        NULL: RawValue
+        CURRENT_TIMESTAMP: RawValue
+        NOW: RawValue
+    }
 }
 
-export declare type QueryResult = ResultSetHeader | any[]
+declare function NodeMysql(options: Mysql.PoolConfig | string): NodeMysql.Pool
 
-export declare interface Streaming {
-    streamQuery(query: string, params?: any): Mysql.Query
-    streamQuery(query: Mysql.QueryOptions): Mysql.Query
-    streamExecute(query: string, params?: any): Mysql.Query
-    streamExecute(query: Mysql.QueryOptions): Mysql.Query
-}
-
-export declare interface PromiseStreaming {
-    streamQuery(query: string, params?: any): Promise<Mysql.Query>
-    streamQuery(query: Mysql.QueryOptions): Promise<Mysql.Query>
-    streamExecute(query: string, params?: any): Promise<Mysql.Query>
-    streamExecute(query: Mysql.QueryOptions): Promise<Mysql.Query>
-}
-
-export declare interface Closable {
-    end(): Promise<void>
-}
-
-export declare interface Queryable extends Mysql.EscapeFunctions {
-    query(query: string, params?: any): Promise<QueryResult>
-    query(query: Mysql.QueryOptions): Promise<QueryResult>
-
-    execute(query: string, params?: any): Promise<QueryResult>
-    execute(query: Mysql.QueryOptions): Promise<QueryResult>
-
-    CONSTRANTS: Constrants
-}
-
-export function escape(value: any, stringifyObjects?: boolean, timeZone?: string): string
-
-export function escapeId(value: string, forbidQualified?: boolean): string
-
-export function format(sql: string, values: any[], stringifyObjects?: boolean, timeZone?: string): string
-
-export function raw(sql: string): () => string
-
-export declare interface TransactionQuery extends Queryable, Streaming, Closable {
-    commit(): Promise<void>
-    rollback(): Promise<void>
-}
-
-export declare type TransactionScope<T> = (conn: Queryable) => Promise<T>
-
-declare interface Pool extends Queryable, Closable, PromiseStreaming {
-    
-}
-
-declare class Pool {
-    beginTransaction(isoLevel?: string): Promise<TransactionQuery>
-    beginTransaction(scope: TransactionScope<T>): Promise<T>
-    beginTransaction(isoLevel: string, scope: TransactionScope<T>): Promise<T>
-}
-
-declare function createPool(options: Mysql.PoolConfig | string): Pool
-
-export declare interface RawValue {
-    toSqlString(): string
-}
-
-declare interface Constrants {
-    NULL: RawValue
-    CURRENT_TIMESTAMP: RawValue
-    NOW: RawValue
-}
-
-export const CONSTRANTS: Constrants
-
-export = createPool
+export = NodeMysql
